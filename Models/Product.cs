2a1daa15ec04bff87838c97e06faa62456926e72
Oleksandr Pavlyk <oleksandr.pavlyk03@gmail.com>
2024-11-13 21:50:24 +0200

    project initialize

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; } 
+        public string Description { get; set; }
+
+        public Product(int id, string name, decimal price, string description)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+    }
+}
